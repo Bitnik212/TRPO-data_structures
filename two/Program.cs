620fa12e3230e21100725115ceda552226d77812
bitnik212 <nnv@bitapp.me>
2021-09-15 15:11:54 +0300

    сделал вторую структуру данных

--- two/Program.cs
+++ two/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace two
+﻿using System;
+
+// просто ходим в двумерном массиве
+// можно искать симвл в строке либо если немного дописать то строку в подстроке
+namespace two
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            string[] strings = {"Москва", "Америка", "USA", "Russia"};
+            string str = "";
+            foreach (var s in strings)
+            {
+                for (var i = 0; i < s.Length; i++)
+                {
+                    str += s[i];
+                }
+                str += "\n";
+            }
+
+            Console.WriteLine(str);
         }
     }
 }
