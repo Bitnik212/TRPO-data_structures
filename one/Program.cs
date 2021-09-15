10afee70689886b6db141f59539f1511bf75e05b
bitnik212 <nnv@bitapp.me>
2021-09-15 14:51:35 +0300

    первая структура данных

--- one/Program.cs
+++ one/Program.cs
@@ -1,9 +1,50 @@
-﻿namespace one
+﻿using System;
+
+namespace one
 {
     internal class Program
     {
+        
+        struct Abobys
+        {
+            public string firstName;
+            public string gender;
+            public int age;
+            
+            private static string[] genders = new[] {"Man", "Woman", "One other gender"};
+            private static string[] names = new[] { "Amir", "Avgustus", "Aboba", "Nikolay", "SerGAY" };
+            
+            public void print()
+            {
+                Console.WriteLine($"name: {firstName}, age: {age}, gender: {gender}");
+            }
+
+            public Abobys getRandomUser()
+            {
+                Random random = new Random();
+                Abobys user = new Abobys();
+                user.age = random.Next(1, 100);
+                user.gender = genders[random.Next(0, genders.Length)];
+                user.firstName = names[random.Next(0, names.Length)];
+                return user;
+            }
+        }
         public static void Main(string[] args)
         {
+
+            Abobys[] users = new[]
+            {
+                new Abobys().getRandomUser(),
+                new Abobys().getRandomUser(),
+                new Abobys().getRandomUser(),
+                new Abobys().getRandomUser(),
+                new Abobys().getRandomUser(),
+                new Abobys().getRandomUser(),
+            };
+            foreach (var abobys in users)
+            {
+                abobys.print();
+            }
         }
     }
 }
